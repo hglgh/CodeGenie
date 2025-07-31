ba04e0adb65f1afd86fdfcd91ca1a78759d70147
HGL <13479874797@163.com>
2025-07-31 10:58:09 +0800

    第三期: 优化代码生成测试并添加流式代码生成测试- 优化 generateAndSaveCode测试方法，改名为 testGenerateAndSaveCode - 添加 testGenerateAndSaveCodeStream 测试方法，用于测试流式代码生成 - 在 pom.xml 中添加 JVM 参数和测试插件配置，解决相关警告问题 - 删除 UserController 中未使用的用户相关 API 接口

--- code-genie-backend/src/test/java/com/hgl/codegeniebackend/core/AiCodeGeneratorFacadeEnhancedTest.java
+++ code-genie-backend/src/test/java/com/hgl/codegeniebackend/core/AiCodeGeneratorFacadeEnhancedTest.java
@@ -5,8 +5,10 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * ClassName: AiCodeGeneratorFacadeEnhancedTest
@@ -23,12 +25,21 @@ class AiCodeGeneratorFacadeEnhancedTest {
     private AiCodeGeneratorFacadeEnhanced aiCodeGeneratorFacadeEnhanced;
 
     @Test
-    void generateAndSaveCode() {
+    void testGenerateAndSaveCode() {
         File file = aiCodeGeneratorFacadeEnhanced.generateAndSaveCode("生成程序员HGL的个人博客", CodeGenTypeEnum.HTML);
         Assertions.assertNotNull(file);
     }
 
     @Test
-    void generateAndSaveCodeStream() {
+    void testGenerateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacadeEnhanced.generateAndSaveCodeStream("生成程序员HGL的个人博客", CodeGenTypeEnum.MULTI_FILE);
+        //阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 测试结果
+        Assertions.assertNotNull(result);
+        //拼接字符串，得到完整内容
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+
     }
 }
